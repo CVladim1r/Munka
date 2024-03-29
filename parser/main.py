cd650b0d094060b209fcbc610c5ebba7688ca088
Extager <greycats.pros@gmail.com>
2024-03-29 03:34:54 +0300

    Aa

--- parser/main.py
+++ parser/main.py
@@ -1,8 +1,13 @@
 import scrapy
+import json
+from scrapy.crawler import CrawlerProcess
 
 class JobSpider(scrapy.Spider):
     name = 'job_spider'
-    start_urls = ['https://spb.hh.ru/']  # Замените на URL сайта с вакансиями
+    start_urls = ['https://spb.hh.ru']  # Замените на URL сайта с вакансиями
+    
+    def __init__(self):
+        self.jobs = []
 
     def parse(self, response):
         # Используем CSS-селекторы или XPath для извлечения информации о вакансиях
@@ -14,20 +19,24 @@ class JobSpider(scrapy.Spider):
             location = job.css('.job-location::text').get()
             salary = job.css('.job-salary::text').get()
 
-            yield {
+            self.jobs.append({
                 'title': title,
                 'company': company,
                 'location': location,
                 'salary': salary,
-            }
+            })
 
         # Если есть следующая страница, переходим к ней
         next_page = response.css('a.next-page::attr(href)').get()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
-# Запускаем парсер
-from scrapy.crawler import CrawlerProcess
+    def closed(self, reason):
+        # Закрытие паука, сохранение данных в файл
+        with open('jobs.json', 'w') as f:
+            json.dump(self.jobs, f)
+
+# Запускаем паука
 
 process = CrawlerProcess()
 process.crawl(JobSpider)
