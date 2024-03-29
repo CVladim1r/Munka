a6ffb9f5f753d8d40578bb779d7aea8eb9ac418d
CVladim1r <vladimir.973@list.ru>
2024-03-29 16:18:01 +0300

    Fix228

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,6 +1,8 @@
 import mysql.connector
-from config import DB_CONFIG
 import logging
+from config import DB_CONFIG
+
+logging.basicConfig(level=logging.INFO)
 
 async def create_connection():
     try:
@@ -9,3 +11,37 @@ async def create_connection():
     except mysql.connector.Error as e:
         logging.error(f"Error connecting to MySQL: {e}")
         return None
+
+async def add_user_to_db(message, user_id, user, user_name, user_type):
+    conn = await create_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            if message.from_user.first_name:
+                user_name = message.from_user.first_name
+            cursor.execute("INSERT INTO users (user_id, name, user_name, user_type) VALUES (%s, %s, %s, %s) "
+                           "ON DUPLICATE KEY UPDATE name=VALUES(name), user_name=VALUES(user_name), user_type=VALUES(user_type)",
+                           (user_id, user, user_name, user_type))
+            conn.commit()
+            logging.info(f"User with ID {user_id} added to the database")
+            cursor.close()
+        except mysql.connector.Error as e:
+            logging.error(f"Error adding user to database: {e}")
+        finally:
+            conn.close()
+
+async def add_user_info_to_db(user_id, name, age, description, company_name):
+    conn = await create_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO users (user_id, user_type, user_name, name, age, description, company_name) VALUES (%s, %s, %s, %s, %s, %s, %s) "
+                           "ON DUPLICATE KEY UPDATE name=VALUES(name), age=VALUES(age), description=VALUES(description), company_name=VALUES(company_name)",
+                           (user_id, None, None, name, age, description, company_name))
+            conn.commit()
+            logging.info(f"User info added to the database for user with ID {user_id}")
+            cursor.close()
+        except mysql.connector.Error as e:
+            logging.error(f"Error adding user info to database: {e}")
+        finally:
+            conn.close()
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import logging
-import mysql.connector
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
+from aiogram.types import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from config import TOKEN, DB_CONFIG
-
-from bot.utils import is_employer
-
+from config import TOKEN
+from database.db_connector import add_user_to_db, add_user_info_to_db
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,69 +14,51 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-
 class UserForm(StatesGroup):
     name = State()
     age = State()
     description = State()
     company_name = State()
 
-
-async def create_connection():
-    try:
-        conn = mysql.connector.connect(**DB_CONFIG)
-        return conn
-    except mysql.connector.Error as e:
-        logging.error(f"Error connecting to MySQL: {e}")
-        return None
-
-
 @dp.message_handler(commands=['start'], state="*")
 async def start(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
     user = message.from_user.username
     user_name = message.from_user.first_name if not message.from_user.username else message.from_user.username
-    await add_user_to_db(message, user_id, user, user_name, None)  # Передаем message в функцию add_user_to_db
-    await message.answer("Привет! Вы сотрудник компании или работодатель?\n\n"
-                         "Для ответа введите 'Соискатель' или 'Работодатель'.")
+    await add_user_to_db(message, user_id, user, user_name, None)
+
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton("Соискатель", callback_data="job_seeker"))
+    keyboard.add(InlineKeyboardButton("Работодатель", callback_data="employer"))
+
+    await message.answer("Привет! Вы сотрудник компании или работодатель?", reply_markup=keyboard)
     await UserForm.next()
 
-async def add_user_to_db(message, user_id, user, user_name, user_type):  # Добавляем message в аргументы функции
-    conn = await create_connection()
-    if conn:
-        try:
-            cursor = conn.cursor()
-            if message.from_user.first_name:
-                user_name = message.from_user.first_name
-            cursor.execute("INSERT INTO users (user_id, name, user_name, user_type) VALUES (%s, %s, %s, %s) "
-                           "ON DUPLICATE KEY UPDATE name=VALUES(name), user_name=VALUES(user_name), user_type=VALUES(user_type)",
-                           (user_id, user, user_name, user_type))
-            conn.commit()
-            logging.info(f"User with ID {user_id} added to the database")
-            cursor.close()
-        except mysql.connector.Error as e:
-            logging.error(f"Error adding user to database: {e}")
-        finally:
-            conn.close()
+@dp.callback_query_handler(lambda c: c.data in ["job_seeker", "employer"], state="*")
+async def process_user_type(callback_query: types.CallbackQuery, state: FSMContext):
+    user_type = callback_query.data
+    await state.update_data(user_type=user_type)
 
+    if user_type == "job_seeker":
+        await callback_query.message.answer("Вы выбрали: Соискатель.")
+    elif user_type == "employer":
+        await callback_query.message.answer("Вы выбрали: Работодатель.")
 
+    await UserForm.next()
 
+@dp.message_handler(state=UserForm.description)
+async def process_description(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = message.text
 
-async def add_user_info_to_db(user_id, name, age, description, company_name):
-    conn = await create_connection()
-    if conn:
-        try:
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO users (user_id, user_type, user_name, name, age, description, company_name) VALUES (%s, %s, %s, %s, %s, %s, %s) "
-                           "ON DUPLICATE KEY UPDATE name=VALUES(name), age=VALUES(age), description=VALUES(description), company_name=VALUES(company_name)",
-                           (user_id, None, None, name, age, description, company_name))
-            conn.commit()
-            logging.info(f"User info added to the database for user with ID {user_id}")
-            cursor.close()
-        except mysql.connector.Error as e:
-            logging.error(f"Error adding user info to database: {e}")
-        finally:
-            conn.close()
+    await UserForm.next()
+    await message.answer("Завершено! Спасибо за регистрацию.")
+
+    async with state.proxy() as data:
+        user_id = message.from_user.id
+        await add_user_info_to_db(user_id, data['name'], data['age'], data['description'], data.get('company_name'))
+
+    await state.finish()
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
