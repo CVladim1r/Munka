3663080340f7798dba240bf0370fefab1235082c
CVladim1r <vladimir.973@list.ru>
2024-04-02 04:15:10 +0300

    Plug-finish

--- plug_bot.py
+++ plug_bot.py
@@ -7,7 +7,6 @@ from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
 from aiogram import executor
 
 from random import uniform
@@ -38,7 +37,7 @@ class CommandState(StatesGroup):
     COMMAND_PROCESSING = State()
 
 async def send_with_interval(message: types.Message, text: str):
-    delay = uniform(1, 2)
+    delay = uniform(0,1)
     for line in text.split('\n\n'):
         if line.strip():
             await asyncio.sleep(delay)
@@ -53,10 +52,11 @@ async def send_welcome(message: types.Message, state: FSMContext):
     
     await send_with_interval(
         message,
+        "Пивет! Давай я расскажу тебе об отом боте:\n\n"
         "В этом боте, сойдутся две стороны, которые постоянно ищут друг друга: соискатель и работодатель.\n \nСоискатель - сможет составить резюме и смотреть вакансии от работодателей.\n \nРаботодатель - сможет размещать вакансии, а так же просматривать резюме от соискателей. \n\n"
         "Просматривая резюме или вакансии, ты сможешь отмечать 👍 понравившиеся тебе и 👎 если тебе что-то не понравилось.\n\n"
         "А теперь, осталось ждать, когда произойдет тот самый ⚡️мэтч⚡️\n\n"
-        "(Если желаете оказать материальную помощь проекту, чтоб он заработал быстрее, то напишите нашему менеджеру, ссылки в профиле) ",
+        "(Если хочешь оказать материальную помощь проекту, чтоб он заработал быстрее, то напишите нашему менеджеру (ссылка в профиле), либо используйте команду /donate)",
     )
     await add_user_to_db_plug_bot(message, message.from_user.id, message.from_user.username, message.from_user.full_name)
     await state.finish()
@@ -66,7 +66,18 @@ async def send_description(message: types.Message, state: FSMContext):
     await CommandState.COMMAND_PROCESSING.set()
     await send_with_interval(
         message,
-        "В этом боте, сойдутся две стороны, которые постоянно ищут друг друга: соискатель и работодатель. 🌚🌝\n\n",
+        "Здесь ты в скором времени сможешь найти или разместить работу в общепите! Офицант, повар, кондитер, менеджер и другие вакансии скоро будут ждать тебя!\n\n",
+    )
+    await state.finish()
+
+
+
+@dp.message_handler(commands=['donate'])
+async def send_description_info(message: types.Message, state: FSMContext):
+    await CommandState.COMMAND_PROCESSING.set()
+    await send_with_interval(
+        message,
+        "Если хочешь поддержать проект финансово, то мы принимаем донаты через sber/tinkoff. Вы можете отправить пожертвование по номеру: tel:+79955934612\n\n",
     )
     await state.finish()
 
@@ -75,10 +86,13 @@ async def send_description_info(message: types.Message, state: FSMContext):
     await CommandState.COMMAND_PROCESSING.set()
     await send_with_interval(
         message,
-        "В этом боте, сойдутся две стороны, которые постоянно ищут друг друга: соискатель и работодатель. 🌚🌝\n\n",
+        "Здесь ты в скором времени сможешь найти работу в общепите! Офицант, повар, кондитер, менеджер и другие вакансии скоро будут ждать тебя!\n\n",
     )
     await state.finish()
 
+@dp.message_handler()
+async def handle_unknown_message(message: types.Message):
+    await message.answer("Извини, я тебя не понял.")
+
 if __name__ == '__main__':
-    from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
