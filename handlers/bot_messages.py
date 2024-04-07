b3e532a89dd7444d83bfc3827066f9ed4c8f632d
Chernikov23 <118567820+Chernikov23@users.noreply.github.com>
2024-04-07 09:51:19 +0300

    Create bot_messages.py

---
+++ handlers/bot_messages.py
@@ -0,0 +1,3 @@
+from aiogram import Router, Bot, F
+from aiogram.types import Message, LabeledPrice, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, PreCheckoutQuery
+from aiogram.filters import Command
