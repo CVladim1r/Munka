6bf6cb855973ba8efb4bb0b764fda6d5107fd36e
CVladim1r <vladimir.973@list.ru>
2024-04-20 16:34:30 +0300

    dsgdsah43

--- bot/handlers/admin/main.py
+++ bot/handlers/admin/main.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 
-# Админ аккаунт в тг боте Munka
+
 def register_admin_handlers(dp: Dispatcher):
-    # todo: register admin handlers
+    # todo: register all admin handlers
     pass
